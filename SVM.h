268205d14d559e94c6227d04ea581a0d31824015
Allen <87592678+AllenChienXXX@users.noreply.github.com>
2023-02-15 16:04:25 -0500

    Update SVM.h

--- SVM.h
+++ SVM.h
@@ -9,16 +9,22 @@ using namespace std;
 class SVM
 {
 public:
+	//constructor
 	SVM(vector<vector<float>>inputs, vector<float>labels float C = 200, float tolerence = 0.0001);
-
+	
+	//initializing alpha and bias vector
 	void init_alpha_b(int len);
 
+	//Calculate the output
 	void get_output();
 
+	//Calculate the error
 	void get_error();
-
+	
+	//Training the model
 	void train(int epoch = 10);
 
+	//update alpha vector with SMO
 	void update();
 
 	//check lowerbound and upperbound
@@ -29,19 +35,26 @@ public:
 
 	//adjust alpha with lower and upperbound
 	void adjust_alpha(float Lbound, float Hbound);
-
+	
+	//update bias
 	void update_bias(int index1, int index2);
-
+	
+	//select alpha through smo
 	int select_alpha1();
-
+	
+	//select alpha with index
 	int select_alpha2(int inde1R);
-
+	
+	//productthe vectors
 	vector<float> v_product(vector<float> v1, vector<float> v2);
-
+	
+	//kernel function, default is linear
 	float Kernel_function(vector<float> x1, vector<float> x2);
 
+	//print out the alphas
 	void print_alpha();
-
+	
+	//print out any vectors
 	void print_v(vector<float> v);
 
 
